23d813911504e65b442c5bf6a3a55476b61d2f7f
zexcv <krusher13@o2.pl>
2024-01-04 19:30:18 +0100

    full_project

---
+++ Todo/Models/ViewModels/TodoViewModel.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+
+namespace Todo.Models.ViewModels
+{
+    public class TodoViewModel
+    {
+        public List<TodoItem> TodoList {get; set;}
+
+        public TodoItem Todo {get; set;}
+    }
+}
